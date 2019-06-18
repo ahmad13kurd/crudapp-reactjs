f21fca992086bcdf38cea0b8f0572af3e7b16b64
ahmad13kurd <evil13kurd@hotmail.com>
2019-06-18 14:45:14 +0200

    Updates

---
+++ precache-manifest.7d17144c56609ffaec393608445791a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c266215ace6f3e67687f942ed0f87003",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eddcc0226281d9029dcb",
+    "url": "/static/css/main.9bda980c.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "eddcc0226281d9029dcb",
+    "url": "/static/js/main.ef1b1efd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

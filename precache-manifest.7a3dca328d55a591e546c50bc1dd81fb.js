a04950ee090837af2b1c3a6d38f1667868e9df5b
ahmad13kurd <evil13kurd@hotmail.com>
2019-06-18 14:51:25 +0200

    Updates

---
+++ precache-manifest.7a3dca328d55a591e546c50bc1dd81fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53e31034ece6f24f04f90b494f02589",
+    "url": "/crudapp-reactjs/index.html"
+  },
+  {
+    "revision": "9266e6227ee3366926b3",
+    "url": "/crudapp-reactjs/static/css/main.9bda980c.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/crudapp-reactjs/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "9266e6227ee3366926b3",
+    "url": "/crudapp-reactjs/static/js/main.d16df9f0.chunk.js"
+  },
+  {
+    "revision": "83ee3fdcd10e0646fb25",
+    "url": "/crudapp-reactjs/static/js/runtime~main.59e3554b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7d17144c56609ffaec393608445791a2.js"
+  "/crudapp-reactjs/precache-manifest.7a3dca328d55a591e546c50bc1dd81fb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crudapp-reactjs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
